88f04bf67418a53d75490903eda8e800afcac4d3
magdymeko456-cell <magdymeko456-cell@users.noreply.github.com>
2026-05-29 11:54:55 +0300

    init: basic structure

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: Scaffold(body: Center(child: Text('Mirror Scorpion'))));
+}
